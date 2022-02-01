b91c75833dd00a26f04b8c26601c9d9108bba7b3
dongminkim0220 <38837579+dongminkim0220@users.noreply.github.com>
2022-02-01 15:28:24 +0900

    Refactoring of EncDec Architecture

--- data/dataset.py
+++ data/dataset.py
@@ -2,8 +2,12 @@ from torch.utils.data import DataLoader, Dataset
 import numpy as np
 
 class SWaTDataset(Dataset):
-    def __init__(self, x, y, x_min, x_max, window_size=1):
+    def __init__(self, x, y, window_size=1):
         super().__init__()
+
+        x_min = np.min(x, 0, keepdims=True)
+        x_max = np.max(x, 0, keepdims=True)
+
         t = (x_min != x_max).astype(np.float32)
         self.x = (x - x_min) / (x_max-x_min + 1e-5) * t
         self.y = y
--- data/load_data.py
+++ data/load_data.py
@@ -4,6 +4,7 @@ import os
 
 
 def load_data(data_path):
+    print(os.getcwd())
     df = pd.read_csv(data_path, index_col = 0)
 
     for var_index in [item for item in df.columns if item != 'Normal/Attack']:
@@ -14,6 +15,26 @@ def load_data(data_path):
     print(df.head())
 
     x = df.values[:,:-1].astype(np.float32)
-    y = (df['Normal/Attack']=='Normal').to_numpy().astype(int)
+    y = (df['Normal/Attack']=='Attack').to_numpy().astype(int)
 
     return x, y
+
+def load_anomaly_intervals(anomaly_labels, window_size):
+    window_y = []
+    for i in range(len(anomaly_labels)):
+        window_y.append(max(anomaly_labels[i:i + window_size]) == 1)
+
+    intervals = []
+    start = None
+    for i, label in enumerate(window_y):
+        if label:
+            if start is None:
+                start = i
+        else:
+            if start is not None:
+                intervals.append((start, i - 1))
+            start = None
+    if start is not None:
+        intervals.append((start, len(window_y) - 1))
+
+    return intervals
